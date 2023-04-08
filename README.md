12682ed0c4332d4e53ca43a55d4303cbe5a9a734
dmitrijmartys <12masek34@mail.ru>
2023-04-08 18:02:03 +0300

    Completed 2 tasks.

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-init
+Ответ на 2 вопрос.
+
+Создал  таблицы:
+
+```
+create table short_names (
+	name_ varchar(50),
+	status integer
+);
+
+create table full_names (
+	name_ varchar(50),
+	status integer
+);
+```
+
+Заполнил данными которые создает скрипт create_sql.py (создает .sql фаил, который надо выполнить в бд).
+Данные созданы как в примере тестового задания, с учетом что вторая таблица не отсортирована.
+
+Запрос который перености статусы из первой таблицы во вторую:
+
+```
+UPDATE full_names
+SET status = short_names.status
+FROM short_names
+WHERE short_names.name_ = split_part(full_names.name_, '.', 1)
+
+```
---
+++ create_sql.py
@@ -0,0 +1,37 @@
+from random import randint, choice, shuffle
+
+
+ext = ('.mp3', '.wav', '.txt', '.tar', '.zip', '.exe', '.xml', '.json', '.sql', '.py', '.js')
+table1 = []
+table2 = []
+
+for index in range(1, 700_001):
+    delimeter = ','
+
+    if index == 700_000:
+        delimeter = ';'
+
+    table1.append(f"('nazvanie{index}', {randint(1,10)}){delimeter}")
+
+
+for index in range(1, 500_001):
+    table2.append(f"('nazvanie{index}{choice(ext)}'),")
+
+shuffle(table2)
+table2[-1] = table2[-1].replace(',', ';')
+
+with open('test.sql', 'w') as f:
+    f.write('truncate short_names;\n')
+    f.write('insert into short_names (name_, status)\n')
+    f.write('values\n')
+
+    for row in table1:
+        f.write(row + '\n')
+
+    f.write('\n\n')
+    f.write('truncate full_names;\n')
+    f.write('insert into full_names (name_)\n')
+    f.write('values\n')
+
+    for row in table2:
+        f.write(row + '\n')
